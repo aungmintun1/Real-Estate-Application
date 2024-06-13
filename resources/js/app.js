925c45391ee7465c0550ae79682fc8120018fe7d
aungmintun1 <92223456+aungmintun1@users.noreply.github.com>
2024-06-13 13:27:24 -0400

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,11 @@
+
+import './jquery-migrate-3.0.0.min';
+import './jquery.mmenu.all';
+import './ace-responsive-menu';
+import './bootstrap-select.min';
+import './parallax';
+import './jquery-scrolltofixed-min';
+import './jquery.counterup';
+import './slider';
+import './pricing-slider';
+import './script'; // Custom script for all pages
