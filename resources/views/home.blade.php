95ea06a08f62bee45a33c2d2a54ec7baa734625b
aungmintun1 <92223456+aungmintun1@users.noreply.github.com>
2024-08-12 22:39:12 -0400

    added links to featured properties

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -230,7 +230,7 @@
 				<div class="col-lg-12">
 					<div class="feature_property_slider">
 						<div class="item">
-							<a href="page-listing-single-v1.html">
+							<a href="/listings/2">
 								<div class="feat_property">
 									<div class="thumb">
 										<img class="img-whp" src="images/property/villa.jpg" alt="fp1.jpg">
@@ -307,7 +307,7 @@
 							</a>
 						</div>
 						<div class="item">
-							<a href="page-listing-single-v3.html">
+							<a href="/listings/21">
 								<div class="feat_property">
 									<div class="thumb">
 										<img class="img-whp" src="images/property/studio.jpg" alt="fp3.jpg">
@@ -319,33 +319,33 @@
 												<li class="list-inline-item"><span class="flaticon-transfer-1 text-white"></span></li>
 												<li class="list-inline-item"><span class="flaticon-heart text-white"></span></li>
 											</ul>
-											<span class="fp_price">$13,000<small>/mo</small></span>
+											<span class="fp_price">$2,500<small>/mo</small></span>
 										</div>
 									</div>
 									<div class="details">
 										<div class="tc_content">
 											<p class="text-thm">Apartment</p>
-											<h4>Renovated Apartment</h4>
-											<p><span class="flaticon-placeholder"></span> 1421 San Pedro St, Los Angeles, CA 90015</p>
+											<h4>Luxury Apartment with City View</h4>
+											<p><span class="flaticon-placeholder"></span>456 Elm St Apt 12</p>
 											<ul class="prop_details mb0">
-												<li class="list-inline-item"><span>Beds: 4</span></li>
-												<li class="list-inline-item"><span>Baths: 2</span></li>
-												<li class="list-inline-item"><span>Sq Ft: 5280</span></li>
+												<li class="list-inline-item"><span>Beds: 2</span></li>
+												<li class="list-inline-item"><span>Baths: 1</span></li>
+												<li class="list-inline-item"><span>Sq Ft: 900</span></li>
 											</ul>
 										</div>
 										<div class="fp_footer">
 											<ul class="fp_meta float-left mb0">
 												<li class="list-inline-item"><span href="#"><img src="images/property/pposter1.png" alt="pposter1.png"></span></li>
-												<li class="list-inline-item"><span href="#">Ali Tufan</span></li>
+												<li class="list-inline-item"><span href="#">Alex</span></li>
 											</ul>
-											<div class="fp_pdate float-right">4 years ago</div>
+											<div class="fp_pdate float-right">3 months ago</div>
 										</div>
 									</div>
 								</div>
 							</a>
 						</div>
 						<div class="item">
-							<a href="page-listing-single-v1.html">
+							<a href="/listings/1">
 								<div class="feat_property">
 									<div class="thumb">
 										<img class="img-whp" src="images/property/white-house.jpg" alt="fp1.jpg">
@@ -384,7 +384,7 @@
 							</a>
 						</div>
 						<div class="item">
-							<a href="page-listing-single-v2.html">
+							<a href="/listings/1">
 								<div class="feat_property">
 									<div class="thumb">
 										<img class="img-whp" src="images/property/condo1.jpg" alt="fp2.jpg">
@@ -422,7 +422,7 @@
 							</a>
 						</div>
 						<div class="item">
-							<a href="page-listing-single-v3.html">
+							<a href="/listings/1">
 								<div class="feat_property">
 									<div class="thumb">
 										<img class="img-whp" src="images/property/alr-1.jpg" alt="fp3.jpg">
@@ -460,7 +460,7 @@
 							</a>
 						</div>
 						<div class="item">
-							<a href="page-listing-single-v1.html">
+							<a href="/listings/1">
 								<div class="feat_property">
 									<div class="thumb">
 										<img class="img-whp" src="images/property/lr-1.jpg" alt="fp1.jpg">
@@ -499,7 +499,7 @@
 							</a>
 						</div>
 						<div class="item">
-							<a href="page-listing-single-v2.html">
+							<a href="/listings/1">
 								<div class="feat_property">
 									<div class="thumb">
 										<img class="img-whp" src="images/property/dr1.jpg" alt="fp2.jpg">
@@ -537,7 +537,7 @@
 							</a>
 						</div>
 						<div class="item">
-							<a href="page-listing-single-v3.html">
+							<a href="/listings/1">
 								<div class="feat_property">
 									<div class="thumb">
 										<img class="img-whp" src="images/property/orange-house.jpg" alt="fp3.jpg">
@@ -575,7 +575,7 @@
 							</a>
 						</div>
 						<div class="item">
-							<a href="page-listing-single-v1.html">
+							<a href="/listings/1">
 								<div class="feat_property">
 									<div class="thumb">
 										<img class="img-whp" src="images/property/bed1.jpg" alt="fp1.jpg">
@@ -614,7 +614,7 @@
 							</a>
 						</div>
 						<div class="item">
-							<a href="page-listing-single-v2.html">
+							<a href="/listings/1">
 								<div class="feat_property">
 									<div class="thumb">
 										<img class="img-whp" src="images/property/view2.jpg" alt="fp2.jpg">
@@ -652,7 +652,7 @@
 							</a>
 						</div>
 						<div class="item">
-							<a href="page-listing-single-v3.html">
+							<a href="/listings/1">
 								<div class="feat_property">
 									<div class="thumb">
 										<img class="img-whp" src="images/property/dr3.jpg" alt="fp3.jpg">
